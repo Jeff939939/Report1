a06414a12927d6dd6c6ed0680c9f0f4b12052640
Jeff939939 <jeffisthebest62@gmail.com>
2024-06-27 12:08:11 +1000

    Update script.js

--- script.js
+++ script.js
@@ -6,24 +6,33 @@ p22 = document.getElementById("p22");
 title = document.getElementById("title"); 
 
 function unlock(){
-console.log("jeff");
-var good=1
-form11.style.display = "none";
-h11.innerHTML = "Peter Dutton annonces that if elected they would instate a ban on social media for under 16's";
-p11.style.display = "none";
-p22.style.display = "block";
-title.innerHTML = "Peter Dutton annonces that if elected they would instate a ban on social media for under 16's";
-document.cookie = "over = 1"
-}
-function lock1(){
-  p22.style.display = "none";
-  console.log("nojeff");
-  good=0;
-  console.log(document.cookie);
-  h11.innerHTML = "You are not 16";
-  p11.style.display = "none";
+  console.log("jeff");
+  var good=1
   form11.style.display = "none";
-  document.cookie = "over = 0"
+  h11.innerHTML = "Peter Dutton annonces that if elected they would instate a ban on social media for under 16's";
+  p11.style.display = "none";
+  p22.style.display = "block";
+  title.innerHTML = "Peter Dutton annonces that if elected they would instate a ban on social media for under 16's";
+  document.cookie = "over = 1"
+  p221()
+  }
+  function lock1(){
+    console.log("nojeff");
+    
+    good=0;
+    console.log(document.cookie);
+    h11.innerHTML = "You are not 16";
+    p11.style.display = "none";
+    form11.style.display = "none";
+    document.cookie = "over = 0"
+  }
+function p222(){
+  console.log("removedp222")
+  p22.style.display = "none";
+}
+function p221(){
+  console.log("removedp221")
+  p22.style.display = "block";
 }
 
 function getData(form) {
