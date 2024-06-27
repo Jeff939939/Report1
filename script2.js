a63b98db245d5cdbb2e21db7ae8661a14f1aed1a
Jeff939939 <jeffisthebest62@gmail.com>
2024-06-27 12:08:36 +1000

    Update script2.js

--- script2.js
+++ script2.js
@@ -3,7 +3,7 @@ h11 = document.getElementById("h11");
 p11 = document.getElementById("p11");
 p22 = document.getElementById("p22"); 
 title = document.getElementById("title"); 
-p22.style.display = "none";
+p222()
 function unlock(){
     console.log("jeff");
     var good=1
@@ -13,10 +13,11 @@ function unlock(){
     p22.style.display = "block";
     title.innerHTML = "Peter Dutton annonces that if elected they would instate a ban on social media for under 16's";
     document.cookie = "over = 1"
+    p221()
     }
     function lock1(){
       console.log("nojeff");
-      p22.style.display = "none";
+      
       good=0;
       console.log(document.cookie);
       h11.innerHTML = "You are not 16";
@@ -24,6 +25,15 @@ function unlock(){
       form11.style.display = "none";
       document.cookie = "over = 0"
     }
+function p222(){
+    console.log("removedp222")
+    p22.style.display = "none";
+}
+function p221(){
+    console.log("removedp221")
+    p22.style.display = "block";
+}
+
 
 if (document.cookie==="over=1"){
     unlock()
